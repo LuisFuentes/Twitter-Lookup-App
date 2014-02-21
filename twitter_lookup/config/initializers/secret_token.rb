b075d2c83fac0072724c37836f31ac5a9797a780
Luis Fuentes <luis90fuentes@gmail.com>
2014-02-21 12:56:02 -0800

    Initial commit. Starting the twitter lookup app.

---
+++ twitter_lookup/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterLookup::Application.config.secret_key_base = 'a765894380afa6b21730070987185dd5993de83d83d23ac0f294108387110a4f34c10681c12dc70790227f6e1cb521a5acff4db9c8c6043b3285ba088726d277'
---
+++ twitter_lookup/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterLookup::Application.config.session_store :cookie_store, key: '_twitter_lookup_session'
